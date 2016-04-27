41dc3d0d4e97f9da1d33fe58b12487eba86a0e48
Robert Rouhani <robert.rouhani@gmail.com>
2016-04-27 03:05:39 -0500

    Added main menu and second scene

---
+++ ACryForBreadAndRoses/Assets/Scripts/Menus/MainMenuScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+
+    private Image fadeImage;
+    private bool startFading, doneFading;
+
+    void Update()
+    {
+        if (startFading)
+        {
+            Color c = fadeImage.color;
+            c.a += 0.01f;
+            fadeImage.color = c;
+
+            if (c.a >= 1.0f)
+            {
+                SceneManager.LoadSceneAsync(1);
+            }
+        }
+    }
+
+    public void StartButton()
+    {
+        startFading = true;
+
+        if (fadeImage == null)
+        {
+            GameObject obj = new GameObject("fadeImage");
+            obj = Instantiate(obj) as GameObject;
+            fadeImage = obj.AddComponent<Image>();
+            obj.GetComponent<RectTransform>().sizeDelta = new Vector2(5000, 5000);
+            obj.transform.SetParent(GameObject.Find("Canvas").transform);
+            //fadeImage = GameObject.Find("Canvas")AddComponent<Image>();
+        }
+
+        fadeImage.color = Color.clear;
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
